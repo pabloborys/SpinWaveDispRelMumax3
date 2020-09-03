db8a8f648b2226df75dcc5bb0b89377b536caef7
pabloborys <38672516+pabloborys@users.noreply.github.com>
2020-09-02 21:25:51 -0300

    Update From_Mumax.jl

--- From_Mumax.jl
+++ From_Mumax.jl
@@ -1,6 +1,6 @@
 using CSV, DataFrames, FFTW, Glob, DSP, Plots
 
-csv_files=glob("m_y*.csv","/home/pabloborys/Documents/AsymmetricDispersion/Processed_Data/Data_Mumax3/Ms_Middle_TG_H25.out/")
+csv_files=glob("m_x*.csv","Path_to_directory_from_mumax3.out/")
 text_files=[]
 for file in csv_files
     open(file,"r") do x
@@ -22,10 +22,10 @@ for i in size(mx_mumax,2)
     mx_mumax[:, i] = mx_mumax[:, i] .* hannt_mumax
 end
 mx_f_k_mumax=abs.(fftshift(fft(mx_mumax)))
-mx_t_k=abs.(fftshift(fft(mx_mumax,[2])))
-mx_f_x=abs.(fftshift(fft(mx_mumax,[1])))
-CSV.write("/home/pabloborys/Documents/AsymmetricDispersion/Processed_Data/Ms/Sim_Mumax/mxt_Ms_Middle_TG_H25.csv",DataFrame(mx_mumax),writeheader=false)
-CSV.write("/home/pabloborys/Documents/AsymmetricDispersion/Processed_Data/Ms/Sim_Mumax/mfk_Ms_Middle_TG_H25.csv",DataFrame(mx_f_k_mumax),writeheader=false)
-CSV.write("/home/pabloborys/Documents/AsymmetricDispersion/Processed_Data/Ms/Sim_Mumax/mxf_Ms_Middle_TG_H25.csv",DataFrame(mx_f_x),writeheader=false)
-CSV.write("/home/pabloborys/Documents/AsymmetricDispersion/Processed_Data/Ms/Sim_Mumax/mkt_Ms_Middle_TG_H25.csv",DataFrame(mx_t_k),writeheader=false)
-#CSV.write("/home/pabloborys/Documents/Mumax3/TempGradient/Co_Pt/mtx_freqband_c_2exc.csv",DataFrame(mx_mumax),writeheader=false)
+mx_t_k_mumax=abs.(fftshift(fft(mx_mumax,[2])))
+mx_f_x_mumax=abs.(fftshift(fft(mx_mumax,[1])))
+CSV.write("Name_of_file_to_save_mx(x,t)",DataFrame(mx_mumax),writeheader=false)
+CSV.write("Name_of_file_to_save_mx(k,f)",DataFrame(mx_f_k_mumax),writeheader=false)
+CSV.write("Name_of_file_to_save_mx(x,f)",DataFrame(mx_f_x_mumax),writeheader=false)
+CSV.write("Name_of_file_to_save_mx(k,t)",DataFrame(mx_t_k_mumax),writeheader=false)
+
